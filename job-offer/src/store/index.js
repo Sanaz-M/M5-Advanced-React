e0b9d5649e874395032e19c4d781455a052243ab
Sanaz M <sanazmohammaddi@gmail.com>
2021-11-29 14:40:51 +0000

    redux persist and transform encrypt

--- job-offer/src/store/index.js
+++ job-offer/src/store/index.js
@@ -2,6 +2,9 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 import favsReducer from '../reducers/favs'
 import jobsReducer from '../reducers/jobs'
 import thunk from 'redux-thunk'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import { encryptTransform } from 'redux-persist-transform-encrypt'
 
 const aComposeThatAlwaysWorks = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
@@ -20,15 +23,29 @@ export const initialState = {
 }
 
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  transforms: [
+    encryptTransform({
+      secretKey: process.env.REACT_APP_ENCRYPT_KEY,
+    }),
+  ],
+}
+
 const bigReducer = combineReducers({
   favs: favsReducer,
   jobs: jobsReducer,
 })
 
+const persistedBigReducer = persistReducer(persistConfig, bigReducer)
+
 const configureStore = createStore(
-  bigReducer,
+  persistedBigReducer,
   initialState,
   aComposeThatAlwaysWorks(applyMiddleware(thunk))
 )
 
+
+export const persistor = persistStore(configureStore)
 export default configureStore
